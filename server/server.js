37b70efa79184963d71030c0de0f57bbf852703c
Adrien Djiongo~ <AdrienDjiongo@gmail.com>
2025-02-02 14:46:09 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -6,17 +6,6 @@ const app = express();
 // ✅ Use CORS middleware before routes
 app.use(cors());
 
-// ✅ If you want to set headers manually
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 app.use(express.json());
 
 res.setHeader("Access-Control-Allow-Origin", "*");
